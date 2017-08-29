c112957f49921d0a3ec6a71d22eb811192b959de
Brian Anderson <brian.anderson@usaa.com>
2017-08-29 17:44:30 -0500

    UPDATING REPO

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+import BooksApp from './App'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+<BrowserRouter>
+  <BooksApp/>
+</BrowserRouter>, document.getElementById('root'))
